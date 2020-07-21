c4f1b0a9af0e53996197668f0c83ecc7309e55c1
Shirlei-Machado <schiufreitas@gmil.com>
2020-07-21 20:36:29 -0300

    check createpost

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,14 @@ import { friendRouter } from "./router/FriendRouter";
 import dotenv from "dotenv";
 import express from "express";
 import { AddressInfo } from "net";
+import { PostRouter } from "./router/PostRouter";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use("/friend", friendRouter);
+app.use("/create", PostRouter);
 
 const server = app.listen(process.env.PORT || 3003, () => {
   if (server) {
