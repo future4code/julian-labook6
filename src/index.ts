5841c8f099dad1d66c1bf4a04127ee7807b33c07
Wendel de Lima <wendel.delima94@gmail.com>
2020-07-21 22:18:57 -0300

    endpoints de Signup e login

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,19 @@ import { friendRouter } from "./router/FriendRouter";
 import dotenv from "dotenv";
 import express from "express";
 import { AddressInfo } from "net";
+import { signupRouter } from "./router/SignupRouter";
+import { loginRouter } from "./router/LoginRouter";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+
 app.use("/friend", friendRouter);
+
+app.use('/signup', signupRouter);
+app.use('/login', loginRouter);
+
 
 const server = app.listen(process.env.PORT || 3003, () => {
   if (server) {
