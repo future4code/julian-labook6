c3dbe0c508d2611b5ca5ac5ae24186906ba56986
Alex <alex.trindade@live.com>
2020-07-23 16:30:13 -0300

    Refatoração de código de login e signup

--- src/router/LoginRouter.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { LoginController } from '../controller/LoginController';
-
-export const loginRouter = express.Router();
-
-loginRouter.post('/', new LoginController().login);
-
